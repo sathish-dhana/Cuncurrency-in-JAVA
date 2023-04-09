adc072831ad74e28885508a131c6cf935ff68ffb
Dhanasekar <dhanasekar.k@nobroker.in>
2023-04-09 09:11:53 +0530

    Executors.newFixedPool example

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,5 @@
+
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+C
+Cuncurrency/pom.xml,f/d/fda62b35441e8b84ce0c689e5d8946f301cd1cc0
---
+++ Cuncurrency/src/main/java/FixedThreadPool/FixedThreadPool.java
@@ -0,0 +1,34 @@
+package FixedThreadPool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class FixedThreadPool {
+
+    /*this we need to use for cpu intensive operation (creating hash function, creating come algorithm)
+    * if we have four core processor this will create only 4 threads
+    * since creating many threads will decrease the performance because once core will be shared by multiple threads
+    * At that time we can use this Executors.newFixedThreadPool to increase efficiency*/
+
+    /*When to use more threads
+    * If you are performing any IO opertaions (like reading files from database, for calling external api for data
+    * our cpu needs to wait for response, this will decrease performance if we run ony one thread in one core
+    * At that time we can increase more threads
+    * this will increase performance since if one thread waits for response, cpu core will execute next thread */
+
+    public static void main(String[] args) {
+        int processors = Runtime.getRuntime().availableProcessors();
+        ExecutorService executorService = Executors.newFixedThreadPool(processors);
+
+        for (int i = 0; i < 10; i++) {
+            executorService.execute(new CpuIntensiveOperation());
+        }
+    }
+
+    public static class CpuIntensiveOperation implements Runnable {
+        @Override
+        public void run() {
+            System.out.println("this is cpu intensive");;
+        }
+    }
+}
